7f3825669c435ee6f726dece176aa5e1d9e7c4ce
Florent Foucaut <florent.foucaut62@gmail.com>
2023-03-22 17:33:23 +0100

    Js de la page login terminer + création de la page user

--- FrontEnd/login.js
+++ FrontEnd/login.js
@@ -1,16 +1,40 @@
-function ajoutListenerLogIn(){
-    const formulaireLogIn = document.querySelector("#login");
-    formulaireLogIn.addEventListener("submit", function(event) {
-        event.preventDefault();
-        const compte = {
-            email: event.target.querySelector("#email").value,
-            password: event.target.querySelector("#password").value
-        };
-    const chargeUtile= JSON.stringify(compte);
-    fetch("http://localhost:5678/api-docs/users/login", {
-        method:"POST",
-        headers: {"Content_Type": "application/json"},
-        body: chargeUtile
-    });
-    });
-}
+// Ajout du listener sur le button du formulaire 
+const loginForm = document.getElementById('login');
+loginForm.addEventListener('submit', function(event) {
+  event.preventDefault(); 
+
+  // Récupération de l'email et du mot de passe
+  const emailInput = document.getElementById('email');
+  const passwordInput = document.getElementById('password');
+
+  // Boucle en cas de champs non rempli
+  const email = emailInput.value;
+  const password = passwordInput.value;
+  if (!email || !password) {
+    alert('Veuillez remplir une email et/ou un mot de passe');
+    return;
+  }
+
+ 
+  const chargeUtile = {
+    email: email,
+    password: password
+  };
+ // Envoie des requêtes à l'api
+  fetch('http://localhost:5678/api-docs/users/login', {
+    method: 'POST',
+    headers: {"Content-type": "application/json"},
+    body: JSON.stringify(chargeUtile)
+  })
+  //Traitement de la réponse de l'API
+  .then(response => {
+    if (response.ok) {
+      alert('Vous êtes maintenant connecté');
+      // Redirige vers la page de l'utilisateur
+      window.location.href = 'user.html';
+    } else {
+      alert('Erreur de connexion');
+    }
+  })
+
+});
